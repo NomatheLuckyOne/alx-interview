dc7f120cca9066eb2facc3573199ed4a1d756d78
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-07-13 18:31:42 +0000

    fixed tab error

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,40 +9,40 @@ def island_perimeter(grid):
 	whole_perimeter = 0
 
 	for idx, row in enumerate(grid):
-        for elem_idx, elements enumerate(row):
-            if elements == 0:
-                continue
+	        for elem_idx, elements in enumerate(row):
+            		if (elements == 0):
+                		continue
 
-            # left
-            if elem_idx != 0 and row[elem_idx - 1] == 0:
-                whole_perimeter += 1
+            		# left
+            		if (elem_idx != 0 and row[elem_idx - 1] == 0):
+                		whole_perimeter += 1
 
-            # left
-            if elem_idx == 0:
-                whole_perimeter += 1
+            		# left
+            		if (elem_idx == 0):
+                		whole_perimeter += 1
 
-            # right
-            if elem_idx != len(row) - 1 and row[elem_idx + 1] == 0:
-                whole_perimeter += 1
-            # right
-            if elem_idx == len(row) - 1:
-                whole_perimeter += 1
+            		# right
+            		if (elem_idx != len(row) - 1 and row[elem_idx + 1] == 0):
+                		whole_perimeter += 1
+           	 	# right
+            		if elem_idx == len(row) - 1:
+                		whole_perimeter += 1
 
-            # top
-            if idx != 0 and grid[idx - 1][elem_idx == 0]:
-                whole_perimeter += 1
-            # top
-            if idx == 0:
-                whole_perimeter += 1
+            		# top
+            		if idx != 0 and grid[idx - 1][elem_idx == 0]:
+                		whole_perimeter += 1
+            		# top
+            		if idx == 0:
+                		whole_perimeter += 1
 
-            # bottom
-            if idx != len(grid) -1 and grid[idx + 1][elem_idx == 0]:
-                whole_perimeter += 1
+            		# bottom
+            		if idx != len(grid) -1 and grid[idx + 1][elem_idx == 0]:
+                		whole_perimeter += 1
 
-            if idx == len(grid) -1:
-                whole_perimeter += 1
+            		if idx == len(grid) -1:
+                		whole_perimeter += 1
 
-    return whole_perimeter
+	return whole_perimeter
 
 
 if __name__ == "__main__":

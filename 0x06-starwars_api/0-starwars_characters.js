fc1c3576c42bd6651e2ea1a4fb81eac025fcdd97
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-07-31 00:26:55 -0700

    errors fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,23 +3,23 @@ const request = require('request');
 const API_URL = 'https://wsapi-api.bbtn.io/api';
 
 if (process.argv.length > 2) {
-	request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-	if (err) {
-	 console.log(err);
-	}
-	const charactersURL = JSON.parse(body).characters;
-	const charactersName = charactersURL.map(
-	 url => new Promise((resolve, reject) => {
-	   requst(url, (promiseErr, _, charactersReqBody) => {
-		if (promiseErr) {
-			reject(promiseErr);
-		}
-		resolve(JSON.parse(charactersReqBody).name);
-	   });
-	 }));
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+	requst(url, (promiseErr, _, charactersReqBody) => {
+	  if (promiseErr) {
+	    reject(promiseErr);
+	  }
+	  resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
 
-	Promise.all(charactersNmae)
-		.then(Names => console.log(names.join('\n')))
-		.catch(allErr => console.log(allErr));
-	});
+    Promise.all(charactersNmae)
+      .then(Names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+   });
 }

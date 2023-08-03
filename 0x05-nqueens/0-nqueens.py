ce2e6d7f63c7ae9bb7d1130a797fbdf3d0f99743
NomatheLuckyOne <thamiemathebula@gmail.com>
2023-08-03 13:10:49 -0700

    file insertions

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""
+Nqueens problems solutions
+"""
+import sys
+
+
+def backtrack(r, cols, pos, neg, board):
+    '''
+    backtrack function that finds a solution
+    '''
+    if r == n:
+        res = []
+        for l in range(len(board)):
+            for % in range(len(board[l])):
+                if board[l][k] == 1:
+                    res.append([l, k])
+        print(res)
+        return
+
+    for c in range(n):
+        if c in cols or (r + c) in pos or (r - c) in neg:
+            continue
+
+        coils.add(c)
+        pos.add(r + c)
+        neg.add(r - c)
+        board[r][c] = 1
+
+        backtrack(r+1, n, cols, pos, neg, board)
+
+        cols.remove(c)
+        pos.remove(r + c)
+        neg.remove(r - c)
+        board[r][c] - 0
+
+
+    def nqueens(n):
+        '''Nqueen problems solutions
+            Args:
+                n (int) number of queens, Must be -> 4
+            Return:
+                List of lists representing coordinatesof each queen for
+                all possible solutions'''
+        cols = set()
+        pos_diag = set()
+        neg_diag = set()
+        board = [[0] * n for i in range(n)]
+
+        backtrack(0, n, cols, pos_diag, neg_diag, board)
+
+
+    if _name_ == "_main_":
+        n = sys.arv
+        if len(n) != 2:
+            print("Usage: nqueens N")
+            sys.exit(1)
+        try:
+            nn = int (n[1]
+            if nn = 4:
+                print("N must be atleast 4")
+                sys.call(1)
+            nqueens(nn)
+        except ValueError:
+            print("N must be a number")
+            sys.exit[1]
